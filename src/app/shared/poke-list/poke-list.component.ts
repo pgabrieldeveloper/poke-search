870365efaea17cca2c6481980d2006794caabf35
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-03-20 17:18:40 -0300

    feat criando filtro de pokemon no search

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -9,13 +9,21 @@ import {Pokemons} from "../models/Pokemons";
 })
 export class PokeListComponent implements OnInit {
   public pokemons : Array<Pokemons> = [];
+  private setPokemons : Array<Pokemons> = []
   constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemon.subscribe(res => {
-      this.pokemons = res.results;
+      this.setPokemons = res.results;
+      this.pokemons = this.setPokemons;
       console.log(this.pokemons);
     })
+  }
+
+  public getSearch(name:string){
+    const pokemonsFiltrados = this.setPokemons.filter( pok => !pok.name.indexOf(name.toLowerCase()));
+    console.log(name);
+    this.pokemons = pokemonsFiltrados;
   }
 
 }
--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-poke-search',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeSearchComponent implements OnInit {
 
+  @Output() public searchEmiter: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public searchEventEmiter(name:string){
+    this.searchEmiter.emit(name);
   }
 
 }
